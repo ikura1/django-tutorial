11d46652518f7dcfac691411c0db52b82afb2bf2
ikura1 <ikura.lab@gmail.com>
2018-12-05 23:16:54 +0900

    feat: modelに関数を追加

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +10,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -16,3 +21,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+

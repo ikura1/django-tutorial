352a1aef888c05b3c89f01a33305e9b6ca7d4c44
ikura1 <ikura.lab@gmail.com>
2018-12-06 20:43:27 +0900

    feat:viewを追加

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You are liiking at the resutls of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}.")

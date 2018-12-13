b1c20c42d271593754294f8124f6bd7ac6284f42
ikura1 <ikura.lab@gmail.com>
2018-12-13 22:51:55 +0900

    text: 質問詳細ページのテストを追加

--- polls/tests.py
+++ polls/tests.py
@@ -74,6 +74,28 @@ class QuestionIndexViewTests(TestCase):
         )
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        公開されていない質問の詳細ページ
+        存在しないため、404を返す
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        公開されている質問の詳細ページ
+        質問の内容が表示される
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
